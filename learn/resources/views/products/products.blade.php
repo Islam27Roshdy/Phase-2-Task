c76ddb4cd426f194fcbbd5605d9913432945ee41
Islam27Roshdy <34521530+Islam27Roshdy@users.noreply.github.com>
2017-12-16 13:12:51 -0800

    Update products.blade.php

--- learn/resources/views/products/products.blade.php
+++ learn/resources/views/products/products.blade.php
@@ -6,17 +6,19 @@
 <li><h3><a href='product/add_new_product'>Add New Product</a></h3></li>
 <li><h3><a href='product/add_new_type'>Add New Type</a></h3></li>
 </ol>
-<h1>Manage all products</h1>
-<table border='3' id='table_existing_products'>
-<tr><td>Product Name</td><td>Amount</td><td>Delete product</td></tr>
+<div id='table_existing_products'>
+<h1 >Manage all products</h1>
+<table border='3' class='tables'>
+<tr><td class='clear'>Product Name</td><td class='clear'>Amount</td><td class='clear'>Delete product</td><td class='clear'>Edit product</td></tr>
 
 @foreach($all_products as $product)
 <tr>
-   <td>{{$product->name}}</td>
-   <td>{{$product->amount}}</td>
-   <td><a href='product/delete/{{$product->id_product}}'>Delete</a></td>
-   <td><a href='edit/{{$product->id_product}}'>Edit</a></td>
+   <td class='clear'>{{$product->name}}</td>
+   <td class='clear'>{{$product->amount}}</td>
+   <td class='clear'><a href='product/delete/{{$product->id_product}}'>Delete</a></td>
+   <td class='clear'><a href='edit/{{$product->id_product}}'>Edit</a></td>
 </tr>
 @endforeach
 </table>
+</div>
 @stop
